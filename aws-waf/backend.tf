d329c0e848083f87e9171b50baa1f888d5e9783e
sandeep <sandeep@seek.com>
2025-06-06 08:31:50 +0000

    updated tf

--- aws-waf/backend.tf
+++ aws-waf/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "seek-infra"
+    organization = "cloud-infra"
 
     workspaces {
       prefix = "waf-cloudconsul-"
